ed101f152fe3e72a91ac3c1ccc8c625e4db7dd20
samarJ19 <samarjoshitech@gmail.com>
2024-05-10 23:44:07 +0530

    backend ready for deployment

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ const serverless = require('serverless-http');
 const cors=require('cors')
 const mainRouter=require("./routes/index")
 const app=express();
-app.use(cors())
+app.use(cors({
+    origin: 'https://vercel.com/samarj19s-projects/payment-frontend/GTWU26QqnvNG8D5J4uy4rHMqZuNX'
+  }));
 app.use(express.json())
-app.use('/.netlify/functions/api/v1',mainRouter);
+app.use('/api/v1',mainRouter);
 const port=process.env.PORT||3000;
 app.listen(port)
 module.exports.handler = serverless(app);
