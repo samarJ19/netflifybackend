84ba6cd93d669b925a5da309dd396b7de285c853
samarJ19 <samarjoshitech@gmail.com>
2024-05-11 00:23:10 +0530

    fixed

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const serverless = require('serverless-http');
+
 const cors=require('cors')
 const mainRouter=require("./routes/index")
 const app=express();
@@ -10,4 +10,4 @@ app.use(express.json())
 app.use('/api/v1',mainRouter);
 const port=process.env.PORT||3000;
 app.listen(port)
-module.exports.handler = serverless(app);
+
