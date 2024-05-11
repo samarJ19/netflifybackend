852a8aef6766a38b9c8c5f0886e9eb8575458960
samarJ19 <samarjoshitech@gmail.com>
2024-05-11 17:48:16 +0530

    cors1

--- index.js
+++ index.js
@@ -3,9 +3,8 @@ const express = require("express");
 const cors=require('cors')
 const mainRouter=require("./routes/index")
 const app=express();
-app.use(cors({
-    origin: 'https://vercel.com/samarj19s-projects/fresh-frontend-for-payments-app/FXPAtNxn8gjNxLYsco1MJUb27hat'
-  }));
+//app.use(cors({ origin: 'https://vercel.com/samarj19s-projects/fresh-frontend-for-payments-app/FXPAtNxn8gjNxLYsco1MJUb27hat'}));
+app.use(cors());
 app.use(express.json())
 app.use('/api/v1',mainRouter);
 const port=process.env.PORT||3000;
